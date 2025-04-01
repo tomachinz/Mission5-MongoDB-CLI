79a2863a778d82d3bdeff87f01fe7e4130827d53
Tom Atkinson @cheese <tom@funk.co.nz>
2025-04-02 02:54:35 +1300

    and this

---
+++ src/debug.js
@@ -0,0 +1,13 @@
+import ConfigModule from "./ConfigModule.js";
+let config = ConfigModule();
+const verbose = config.get('verbose');
+function debug(s) {
+        if (verbose === true) {
+            console.error(s)
+            if (typeof s == "[object Object]") {
+                console.log(Reflect.getPrototypeOf(s))
+            }
+        }
+    }
+
+    export default debug;
